8c6552ad69804f321e5ab30e526582aba1336092
Kyle H <ikyle53@icloud.com>
2022-03-24 16:40:01 -0700

    Finished all TODO's up until we got to the table

--- js/app.js
+++ js/app.js
@@ -7,18 +7,18 @@ const Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // DONE: Fill in this instance method to create a new CartItem and add it to this.items
   this.items.push(new CartItem(product, quantity));
 };
 
 Cart.prototype.saveToLocalStorage = function() {
-  // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  // DONE: Fill in this instance method to save the contents of the cart to localStorage
   let stringifiedCart = JSON.stringify(this.items);
   localStorage.setItem('cartItem', stringifiedCart);
 };
 
 Cart.prototype.removeItem = function(item) {
-  // TODO: Fill in this instance method to remove one item from the cart.
+  // DONE: Fill in this instance method to remove one item from the cart.
     for (let i = 0; i < cart.length; i += 1){
       if (cart[i].products === products){
         cart.splice(i,1);
@@ -29,7 +29,7 @@ Cart.prototype.removeItem = function(item) {
   // Note: You will have to what kind of parameter to pass in here!
 };
 
-const CartItem = function(product, quantity) { //gets quantity
+const CartItem = function(product, quantity) {
   this.product = product;
   this.quantity = quantity;
 };
